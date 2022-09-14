ac3ca4236806d3aa834952fb592885a85fa77169
MansiKDolia <113482628+MansiKDolia@users.noreply.github.com>
2022-09-13 20:06:21 -0400

    Add files via upload

---
+++ start.py
@@ -0,0 +1,48 @@
+print("Hello World")
+number1=2
+number2=3
+number3= number1+number2
+print("The sum is :",number3)
+
+name= None
+print(name)
+
+PI=3.14
+print (PI)
+
+my_float_var=3.5
+my_integer_var=1000
+my_complex_var=1+2j
+print(type(my_float_var))
+print(type(my_complex_var))
+print(type(my_integer_var))
+
+my_name='Mansi'
+print(my_name)
+
+power=3**3
+print(power)
+
+remainder=8%3
+print(remainder)
+
+
+string2= 'Mansi' + 'Dolia'
+string3='Mansi'
+string4='Dolia'
+string3 += string4
+print(string3)
+print(string2)
+
+print("please enter the required numbers")
+num1=int(input())
+num2=int(input())
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+power=num1**num2
+print(add,sub,mul,div,power)
+
+
+
